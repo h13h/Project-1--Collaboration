ffe852d4220da18c0c047fad14ca4ce32e68fca5
KinjalPatel252617 <kvinseepatel@gmail.com>
2024-10-30 13:47:02 -0400

    portfolio button for about page

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ window.addEventListener('scroll', function() {
     const header = document.getElementById('main-header');
     header.style.backgroundColor = window.scrollY > 50 ? '#555' : '#333';
 });
+
+const portfolioButton = document.getElementById('portfolio-button');
+if (portfolioButton) {
+    portfolioButton.addEventListener('click', () => window.location.href = 'about.html');
+}
+
